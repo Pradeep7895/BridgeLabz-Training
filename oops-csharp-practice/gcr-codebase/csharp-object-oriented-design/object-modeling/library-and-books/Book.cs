f018575eb1ca3512758c70bec6297f3b2ef801e1
Pradeep7895 <pradeepchaudhary789591@gmail.com>
2026-01-04 23:16:01 +0530

    [Pradeep] Add.Added object oriented design codes

---
+++ oops-csharp-practice/gcr-codebase/csharp-object-oriented-design/object-modeling/library-and-books/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.oops_csharp_practice.gcr_codebase.csharp_object_oriented_design.object_modeling.library_and_books
+{
+    internal class Book
+    {
+        //book title
+        public string Title { get; private set; }
+        //book author
+        public string Author { get; private set; }
+
+        //constructor
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author;
+        }
+        //method to display info.
+        public void DisplayBook()
+        {
+            Console.WriteLine($"Book: {Title}, Author: {Author}");
+        }
+    }
+}
---
+++ oops-csharp-practice/gcr-codebase/csharp-object-oriented-design/object-modeling/library-and-books/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.oops_csharp_practice.gcr_codebase.csharp_object_oriented_design.object_modeling.library_and_books
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Books created independently
+            Book book1 = new Book("Clean Code", "Robert C. Martin");
+            Book book2 = new Book("The Java Programmer", "Andrew Hunt");
+
+            // Libraries
+            Library cityLibrary = new Library("GLA Library");
+            Library collegeLibrary = new Library("College Library");
+
+            // Same book added to multiple libraries
+            cityLibrary.AddBook(book1);
+            cityLibrary.AddBook(book2);
+            collegeLibrary.AddBook(book1);
+
+            cityLibrary.ShowBooks();
+            collegeLibrary.ShowBooks();
+        }
+    }
+}
