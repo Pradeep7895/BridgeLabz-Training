f4c9f1c51721e36da898549e41ed30ebee253c37
Pradeep7895 <pradeepchaudhary789591@gmail.com>
2026-01-05 23:32:41 +0530

    [Pradeep] Add.Added vehicle rental application codes

---
+++ oops-csharp-practice/scenario-based/vehicle-rental-application/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.oops_csharp_practice.scenario_based.vehicle_rental_application
+{
+    internal class Car : Vehicle , IRentable
+    {
+        //constructor for child class car
+        public Car(int id, string brand, double rentPerDay)
+           : base(id, brand, rentPerDay)
+        {
+        }
+        //method to calculate rent
+        public double CalculateRent(int days)
+        {
+            return rentPerDay * days;
+        }
+
+        //toString method to display details
+        public override string ToString()
+        {
+            return base.ToString() + "\nVehicle Type: Car";
+        }
+    }
+}
