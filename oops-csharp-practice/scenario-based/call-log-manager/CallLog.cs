0bb3bb4fa910611d6f67930efbee0965f8ae10c4
Pradeep7895 <pradeepchaudhary789591@gmail.com>
2026-01-06 16:39:56 +0530

    [Pradeep] Add.Added customer service call log manager code

---
+++ oops-csharp-practice/scenario-based/call-log-manager/CallLog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.oops_csharp_practice.scenario_based.call_log_manager
+{
+    internal class CallLog
+    {
+        private string phoneNumber;
+        private string message;
+        private DateTime timestamp;
+
+        //constructor
+        public CallLog(string phoneNumber, string message, DateTime timestamp)
+        {
+            this.phoneNumber = phoneNumber;
+            this.message = message;
+            this.timestamp = timestamp;
+
+        }
+        //getter and setter
+        //for phone  number
+        public string GetPhoneNumber() { return phoneNumber; }
+        public void SetPhoneNumber(string phoneNumber) { this.phoneNumber = phoneNumber; }
+
+        //for message
+        public string GetMessage() { return message; }
+        public void SetMessage(string message) {  this.message = message; }
+        
+        //for timestamp
+        public DateTime GetTimestamp() { return timestamp; }
+        public void SetTimestamp(DateTime timestamp) { this.timestamp = timestamp; }
+
+        //ToString method override
+        public override string ToString()
+        {
+            return $"Phone Number : {phoneNumber}\n" +
+                $"Message : {message}\n" +
+                $"Time : {timestamp}";
+        }
+    }
+}
---
+++ oops-csharp-practice/scenario-based/call-log-manager/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.oops_csharp_practice.scenario_based.call_log_manager
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            CallManager manager = new CallManager(10);
+
+            //add call logs
+            manager.AddCallLog(new CallLog("7895914026", "Internet speed related problem", DateTime.Now.AddHours(-4)));
+            manager.AddCallLog(new CallLog("7878435347", "Network speed related problem", DateTime.Now.AddHours(-2.5)));
+            manager.AddCallLog(new CallLog("1234567890", "Network issue problem in mobile phone", DateTime.Now.AddHours(-2)));
+            manager.AddCallLog(new CallLog("9876543210", "Too much spam messages issue", DateTime.Now.AddHours(-1.5)));
+            manager.AddCallLog(new CallLog("7893455026", "Billing related problem", DateTime.Now.AddHours(-1)));
+
+            //search by keyword 
+            manager.SearchByKeyword("speed");
+
+            DateTime start = DateTime.Now.AddHours(-2.5);
+            DateTime end = DateTime.Now;
+
+            //filter by time
+            manager.FilterByTime(start, end);
+        }
+    }
+}
