ff7f7a45fb097ea72f6c216e8e2bd7577252e0f5
Pradeep7895 <pradeepchaudhary789591@gmail.com>
2026-01-07 16:31:18 +0530

    [Pradeep] Add.UC-4 added updated menu

--- oops-csharp-practice/scenario-based/employee-management-system/EmployeeMenu.cs
+++ oops-csharp-practice/scenario-based/employee-management-system/EmployeeMenu.cs
@@ -6,43 +6,53 @@ using System.Threading.Tasks;
 
 namespace BridgeLabzTraining.oops_csharp_practice.scenario_based.employee_management_system
 {
-    
-    sealed class EmployeeMenu 
+
+    sealed class EmployeeMenu
     {
         private IEmployee employeeUtility;
 
         public void Choice()
         {
-            // Assign implementation to interface reference
             employeeUtility = new EmployeeUtilityImpl();
+            bool exit = false;
 
-            Console.WriteLine("Employee Menu: ");
-            Console.WriteLine("1. Check Attendance");
-            Console.WriteLine("2. Calculate Daily Wage");
-            Console.WriteLine("3. Calculate Part-Time Wage");
-            Console.Write("Enter your choice: ");
-
-            int choice = int.Parse(Console.ReadLine());
-
-            switch (choice)
+            while (!exit)
             {
-                case 1:
-                    Employee emp = employeeUtility.CheckAttendance();
-                    Console.WriteLine("Result :");
-                    Console.WriteLine(emp);
-                    break;
-                case 2:
-                    Employee emp2 = employeeUtility.CalculateDailyWage();
-                    Console.WriteLine("Result :");
-                    Console.WriteLine(emp2);
-                    break;
-                case 3:
-                    Console.WriteLine("Result");
-                    Console.WriteLine(employeeUtility.CalculatePartTimeWage());
-                    break;
-                default:
-                    Console.WriteLine("Invalid Choice");
-                    break;
+                Console.WriteLine("\nEmployee Menu :");
+                Console.WriteLine("1. Check Attendance");
+                Console.WriteLine("2. Calculate Daily Wage");
+                Console.WriteLine("3. Calculate Part-Time Wage");
+                Console.WriteLine("0. Exit");
+                Console.Write("Enter your choice: ");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("\nResult: ");
+                        Console.WriteLine(employeeUtility.CheckAttendance());
+                        break;
+
+                    case 2:
+                        Console.WriteLine("\nResult: ");
+                        Console.WriteLine(employeeUtility.CalculateDailyWage());
+                        break;
+
+                    case 3:
+                        Console.WriteLine("\nResult: ");
+                        Console.WriteLine(employeeUtility.CalculatePartTimeWage());
+                        break;
+
+                    case 0:
+                        exit = true;
+                        Console.WriteLine("Exit Menu.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Choice.");
+                        break;
+                }
             }
         }
     }
