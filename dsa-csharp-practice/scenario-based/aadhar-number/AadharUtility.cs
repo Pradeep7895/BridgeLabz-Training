cc29a31c55cc4706ecf70dd73b4ba99876388335
Pradeep7895 <pradeepchaudhary789591@gmail.com>
2026-01-20 16:48:55 +0530

    [Pradeep] Add.Added aadhar numbers sort nad search scenario based code

---
+++ dsa-csharp-practice/scenario-based/aadhar-number/AadharUtility.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.dsa_csharp_practice.scenario_based.aadhar_number
+{
+    internal class AadharUtility
+    {
+        private AadharRecord[] records;
+        private RadixSorting sorter = new RadixSorting();
+        private Searching searcher = new Searching();
+
+        public void AddAadharDetails()
+        {
+            Console.WriteLine("Enter number of entries of aadhar: ");
+            int n = int.Parse(Console.ReadLine());
+
+            records = new AadharRecord[n];
+
+            for(int i=0; i<n; i++)
+            {
+                while (true)
+                {
+                    Console.WriteLine("Enter aadhar holder name: ");
+                    string name = Console.ReadLine();
+
+                    Console.WriteLine("Enter 12 digit Aadhar number: ");
+                    string number = Console.ReadLine();
+
+                    if(number.Length == 12 && long.TryParse(number , out long num))
+                    {
+                        records[i] = new AadharRecord(name, num);
+                        break;
+                    }
+                    Console.WriteLine("Invalid Aadhar.");
+                }
+            }
+            Console.WriteLine("Aadhar Details Added sucessfully.");
+        }
+
+        public void SortAadharNum()
+        {
+            sorter.Sort(records);
+            Console.WriteLine("Aadhar number sorted successfully.");
+        }
+
+        public void SearchAadhar()
+        {
+            Console.WriteLine("Enter Aadhar number : ");
+            long target = long.Parse(Console.ReadLine());
+
+            int index = searcher.Search(records, target);
+
+            if(index != -1)
+            {
+                Console.WriteLine($"Aadhar found at position: " + ( index + 1));
+            }
+            else
+            {
+                Console.WriteLine("Aadhar not found");
+            }
+        }
+        public void Display()
+        {
+            Console.WriteLine("Aadhar records : ");
+            foreach (var item in records)
+            {
+                Console.WriteLine("Aadhar Holder Name : " + item.GetName() + " | " + "Aadhar Number :" + item.GetNumber());
+            }
+        }
+
+    }
+}
---
+++ dsa-csharp-practice/scenario-based/aadhar-number/Menu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.dsa_csharp_practice.scenario_based.aadhar_number
+{
+    internal class Menu
+    {
+        public void Start()
+        {
+            AadharUtility utility = new AadharUtility();
+            int choice;
+
+            do
+            {
+                Console.WriteLine("\n====== AADHAR MANAGEMENT ======");
+                Console.WriteLine("1. Add Aadhar Details");
+                Console.WriteLine("2. Display Aadhar Numbers");
+                Console.WriteLine("3. Sort Aadhar Numbers (Radix Sort)");
+                Console.WriteLine("4. Search Aadhar Number (Binary Search)");
+                Console.WriteLine("0. Exit");
+                Console.Write("Enter your choice: ");
+
+                choice = int.Parse(Console.ReadLine());
+
+                switch(choice)
+                {
+                    case 1:
+                        utility.AddAadharDetails();
+                        break;
+                    case 2:
+                        utility.Display();
+                        break;
+                    case 3:
+                        utility.SortAadharNum();
+                        break;
+                    case 4:
+                        utility.SearchAadhar();
+                        break;
+                    case 0:
+                        Console.WriteLine("Exit...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break ;
+                }
+
+            } while (choice != 0);
+        }
+    }
+}
