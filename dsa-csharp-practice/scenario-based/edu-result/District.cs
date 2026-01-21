cb4bdf4b07e449c80091dcf63bd5a48c3d05d419
Pradeep7895 <pradeepchaudhary789591@gmail.com>
2026-01-21 16:47:27 +0530

    [Pradeep] Add.Added rank sheet generator scenario based codes

---
+++ dsa-csharp-practice/scenario-based/edu-result/District.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.senario_based.EduResult
+{
+    internal class District
+    {
+        public int districtId ;
+        public List<StudentMarks> districtStudents = new List<StudentMarks>();
+
+        public District(int id, List<StudentMarks> s)
+        {
+            this.districtId = id;
+            this.districtStudents = s;
+        }
+        public void Sort()
+        {
+            districtStudents.Sort((a, b) => b.GetMarks().CompareTo(a.GetMarks()));
+        }
+
+        public void Display()
+        {
+            foreach(StudentMarks i in districtStudents)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ dsa-csharp-practice/scenario-based/edu-result/StudentMarks.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.senario_based.EduResult
+{
+    internal class StudentMarks
+    {
+        protected string studentName;
+        protected double marks;
+        
+        //constructor
+        public StudentMarks(string name, double marks)
+        {
+            this.studentName = name;
+            this.marks = marks;
+        }
+
+        //public getter methoda to access fields
+        public string GetStudentName() { return studentName; }
+        public double GetMarks() { return marks; }
+
+        //TOString method to display details
+        public override string ToString()
+        {
+            return "Name: " + studentName + "\nMarks: " + marks;
+        }
+    }
+}
