6eed4500fa002d5eefcbdd02000645f7b2f5a3cf
Pradeep7895 <pradeepchaudhary789591@gmail.com>
2026-01-28 02:08:31 +0530

    [Pradeep] Add.Added reflections assignment codes

---
+++ collection-csharp-practice/gcr-codebase/reflection/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+
+
+namespace Collections.collection_csharp_practice.gcr_codebase.reflection
+{
+    class Calculator
+    {
+        private int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Calculator calc = new Calculator();
+            Type type = typeof(Calculator);
+
+            MethodInfo method = type.GetMethod(
+                "Multiply",
+                BindingFlags.NonPublic | BindingFlags.Instance);
+
+            int result = (int)method.Invoke(calc, new object[] { 5, 4 });
+
+            Console.WriteLine("Multiplication Result: " + result);
+        }
+    }
+}
