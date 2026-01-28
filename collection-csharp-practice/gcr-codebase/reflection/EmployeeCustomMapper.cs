f682c1a6755b110d7daf502c7726d56125d88aa1
Pradeep7895 <pradeepchaudhary789591@gmail.com>
2026-01-28 09:48:37 +0530

    [Pradeep] Add.Added reflections assignment codes

---
+++ collection-csharp-practice/gcr-codebase/reflection/EmployeeCustomMapper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Reflection;
+
+
+
+namespace Collections.collection_csharp_practice.gcr_codebase.reflection
+{
+    public class Employee
+    {
+        public int Id;
+        public string Name;
+        public double Salary;
+    }
+
+    //object mapping
+    public static class ObjectMapper
+    {
+        public static T ToObject<T>(Dictionary<string, object> properties)
+            where T : new()
+        {
+            T obj = new T();
+            Type type = typeof(T);
+
+            foreach (var item in properties)
+            {
+                FieldInfo field = type.GetField(item.Key);
+                if (field != null)
+                {
+                    field.SetValue(obj, item.Value);
+                }
+            }
+            return obj;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var data = new Dictionary<string, object>
+        {
+            { "Id", 1 },
+            { "Name", "Pradeep" },
+            { "Salary", 50000.0 }
+        };
+
+            Employee emp = ObjectMapper.ToObject<Employee>(data);
+            Console.WriteLine($"{emp.Id}, {emp.Name}, {emp.Salary}");
+        }
+    }
+
+}
