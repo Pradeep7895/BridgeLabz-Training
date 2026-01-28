5eb62b2721f5e9a89ed2d7ba1c139acab115d161
Pradeep7895 <pradeepchaudhary789591@gmail.com>
2026-01-28 09:10:18 +0530

    [Pradeep] Add.Added reflections assignment codes

---
+++ collection-csharp-practice/gcr-codebase/reflection/Configuration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+//Access and Modify Private Static Fields
+
+namespace Collections.collection_csharp_practice.gcr_codebase.reflection
+{
+    class Configuration
+    {
+        private static string API_KEY = "OLD_KEY";
+
+        public static void ShowKey()
+        {
+            Console.WriteLine("API KEY: " + API_KEY);
+        }
+    }
+
+    //reflection code to modify static field
+    class Program
+    {
+        static void Main()
+        {
+            Type type = typeof(Configuration);
+
+            FieldInfo field = type.GetField("API_KEY", BindingFlags.NonPublic | BindingFlags.Static);
+
+            // Print old value
+            Configuration.ShowKey();
+
+            // Modify static field
+            field.SetValue(null, "NEW_SECURE_KEY");
+
+            // Print new value
+            Configuration.ShowKey();
+        }
+    }
+}
