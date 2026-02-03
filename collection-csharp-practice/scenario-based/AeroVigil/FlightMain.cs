33db2efe7becf7de522adbce53e8aed08569b2dc
Pradeep7895 <pradeepchaudhary789591@gmail.com>
2026-02-03 11:54:23 +0530

    [Pradeep] Add.Added scenario based codes

---
+++ collection-csharp-practice/scenario-based/AeroVigil/FlightMain.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.collection_csharp_practice.senario_based.AeroVigil
+{
+    internal class FlightEntry
+    {
+        public static void Main(String[] args)
+        {
+            UserInterface user = new UserInterface();
+            user.Start();
+        }
+    }
+}
