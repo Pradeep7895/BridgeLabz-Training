d88f52d8c31e9fae1d9bcc4f174d49e2b378b2bd
Pradeep7895 <pradeepchaudhary789591@gmail.com>
2026-02-03 12:47:47 +0530

    [Pradeep] Add.Added scenario based codes

---
+++ collection-csharp-practice/scenario-based/StreamBuzz/IEngagement.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.senario_based.StreamBuzz
+{
+    internal interface IEngagement
+    {
+        void RegisterCreator(CreatorStats record);
+        Dictionary<string, int> GetTopPostCounts(List<CreatorStats> records, double likeThreshold);
+        double CalculateAverageLikes();
+    }
+}
