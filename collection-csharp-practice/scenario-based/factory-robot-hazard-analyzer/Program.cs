2ea46f00d84de476a6d78093ff2a43be884b321b
Pradeep7895 <pradeepchaudhary789591@gmail.com>
2026-02-04 02:06:52 +0530

    [Pradeep] Add.Added factory robot hazard analyzer custom exception scenario based codes

---
+++ collection-csharp-practice/scenario-based/factory-robot-hazard-analyzer/Program.cs
@@ -0,0 +1,11 @@
+namespace Collections.collection_csharp_practice.senario_based
+{
+    public class Program
+    {
+        static void Main()
+        {
+            RobotHazardAuditor r1 = new RobotHazardAuditor();
+            r1.Entry();
+        }
+    }
+}
